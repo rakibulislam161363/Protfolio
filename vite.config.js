4c4723183016861a827bde4ffd1db22737105aa0
rakibulislam161363 <rakibulislam161363@gmail.com>
2024-12-23 20:41:39 +0600

    again done

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'script.js'),
+        main: resolve(__dirname, 'style.css'),
+        main: resolve(__dirname, 'testing.css'),
+      },
+    },
+  },
+});
